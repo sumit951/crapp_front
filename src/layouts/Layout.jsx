20ef18a96367977a10a108158ae9eb904cb41c79
sumit951 <sumit.kumar@360websitedesigning.com>
2025-07-04 17:09:55 +0530

    header path resolved

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { Outlet, useNavigate } from "react-router-dom";
-import Header from "../components/header";
+import Header from "../components/Header";
 import Sidebar from "../components/Sidebar";
 
 export default function Layout() {
